879938df473382953de1914f1e943cd7e6622b1e
Μάρκος Γιακουμής <mgiako02@ucy.ac.cy>
2026-02-09 20:50:58 +0200

    Force core in q7

--- scripts/q7.sh
+++ scripts/q7.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 make clean && make OPTIMIZE=O0
-perf stat -e instructions,cycles,cache-references,cache-misses,branch-instructions,branch-misses ./build/bin/kmeans ./images/input.jpg ./out/q7O0output5.jpg 5
+perf stat -e instructions,cycles,cache-references,cache-misses,branch-instructions,branch-misses taskset -c 1 ./build/bin/kmeans ./images/input.jpg ./out/q7O0output5.jpg 5
 make clean && make OPTIMIZE=O3
-perf stat -e instructions,cycles,cache-references,cache-misses,branch-instructions,branch-misses ./build/bin/kmeans ./images/input.jpg ./out/q7O3output5.jpg 5
+perf stat -e instructions,cycles,cache-references,cache-misses,branch-instructions,branch-misses taskset -c 1 ./build/bin/kmeans ./images/input.jpg ./out/q7O3output5.jpg 5
